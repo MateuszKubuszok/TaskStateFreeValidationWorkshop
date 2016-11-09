f631ebad54f72fabed3d92fbdce5c6c23a05bc96
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2016-11-09 18:12:06 +0100

    Gave up on Loggers

--- modules/validation/src/main/scala/io/scalawave/workshop/validation/Main.scala
+++ modules/validation/src/main/scala/io/scalawave/workshop/validation/Main.scala
@@ -1,29 +1,28 @@
 package io.scalawave.workshop.validation
 
-import io.scalawave.workshop.common.Logging
-
 import scala.annotation.tailrec
 
-object Main extends Logging {
+object Main {
 
   def main(args: Array[String]): Unit = {
-    logger info "Run scalaz validation"
+    println("Run scalaz validation")
     val scalazConfig = testScalaz
-    logger info s"Config resolved to $scalazConfig"
-    logger info "Run cats validation"
+    println(s"Config resolved to $scalazConfig")
+    println()
+    println("Run cats validation")
     val catsConfig = testCats
-    logger info s"Config resolved to $catsConfig"
+    println(s"Config resolved to $catsConfig")
   }
 
   @tailrec
   def testScalaz: Config = {
-    logger info "Enter accuracy:"
+    println("Enter accuracy: ")
     val accuracy = scala.io.StdIn.readLine
-    logger info "Enter data source:"
+    println("Enter data source: ")
     val dataSource = scala.io.StdIn.readLine
     ScalazValidation.parse(accuracy, dataSource) match {
       case scalaz.Failure(errors) =>
-        errors.foreach { error => logger error error.toString }
+        errors.foreach { error => println(error) }
         testScalaz
       case scalaz.Success(value) => value
     }
@@ -31,13 +30,13 @@ object Main extends Logging {
 
   @tailrec
   def testCats: Config = {
-    logger info "Enter accuracy:"
+    println("Enter accuracy: ")
     val accuracy = scala.io.StdIn.readLine
-    logger info "Enter data source:"
+    println("Enter data source: ")
     val dataSource = scala.io.StdIn.readLine
     CatsValidation.parse(accuracy, dataSource) match {
       case cats.data.Validated.Invalid(errors) =>
-        errors.toList.foreach { error => logger error error.toString }
+        errors.toList.foreach { error => println(error) }
         testCats
       case cats.data.Validated.Valid(value) => value
     }
