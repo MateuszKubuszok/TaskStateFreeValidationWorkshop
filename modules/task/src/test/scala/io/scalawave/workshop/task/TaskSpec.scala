63e2f62ba25393455b35cb85f06d66abc611c6fc
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2016-11-18 16:16:07 +0100

    General test fixes

--- modules/task/src/test/scala/io/scalawave/workshop/task/TaskSpec.scala
+++ modules/task/src/test/scala/io/scalawave/workshop/task/TaskSpec.scala
@@ -1,6 +1,5 @@
 package io.scalawave.workshop.task
 
-import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 
-class TaskSpec extends Specification with Mockito
+class TaskSpec extends Specification
--- modules/warmup/src/test/scala/io/scalawave/workshop/warmup/WarmupSpec.scala
+++ modules/warmup/src/test/scala/io/scalawave/workshop/warmup/WarmupSpec.scala
@@ -1,6 +1,5 @@
 package io.scalawave.workshop.warmup
 
-import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 
-class WarmupSpec extends Specification with Mockito
+class WarmupSpec extends Specification
