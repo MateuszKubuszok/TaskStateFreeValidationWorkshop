6368ef1310bdc2c42da9a5f6f931df56e5c64a24
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2016-11-19 17:25:03 +0100

    State excercise exercises

--- modules/state/src/main/scala/io/scalawave/workshop/state/CurrencyExchangeRates.scala
+++ modules/state/src/main/scala/io/scalawave/workshop/state/CurrencyExchangeRates.scala
@@ -3,6 +3,11 @@ package io.scalawave.workshop.state
 import io.scalawave.workshop.common.Currency
 import Currency.Currency
 
+/** Demo exchange rate provider
+  *
+  * This class is used by the demo to provide some basic conversion features. It is demo only though - in proper program
+  * we would use an **actual** data source.
+  */
 case class CurrencyExchangeRates(dollarExchangeRates: Map[Currency, Double] = Map.empty) {
 
   def getRate(from: Currency, to: Currency): Option[Double] = for {
--- modules/state/src/main/scala/io/scalawave/workshop/state/ScalazState.scala
+++ modules/state/src/main/scala/io/scalawave/workshop/state/ScalazState.scala
@@ -5,18 +5,41 @@ import Currency.Currency
 
 import scalaz._
 
+/** Direct state manipulation - Scalaz
+  *
+  * Here we only want to show some of the most generic State functions - ones will allows to extract some data from
+  * passed state, perform calculations and return potentially modified state as well as possible result of those
+  * calculations.
+  */
 object ScalazState {
 
+  /** Preserves state and returns calculated exchange rate.
+    *
+    * @param from conversion from
+    * @param to conversion to
+    * @return state -> calculated exchange rate
+    */
   def getExchangeRate(from: Currency, to: Currency): State[CurrencyExchangeRates, Option[Double]] =
     State[CurrencyExchangeRates, Option[Double]] { state =>
       state -> state.getRate(from, to)
     }
 
+  /** Preserves state and returns USD exchange rate.
+    *
+    * @param to conversion to
+    * @return state -> exchange rate
+    */
   def getDollarExchangeRate(to: Currency): State[CurrencyExchangeRates, Option[Double]] =
     State[CurrencyExchangeRates, Option[Double]] { state =>
       state -> state.dollarExchangeRates.get(to)
     }
 
+  /** Updates state with new USD exchange rate and returns old rate.
+    *
+    * @param to conversion to
+    * @param rate exchange rate
+    * @return new state -> old exchange rate
+    */
   def setDollarExchangeRate(to: Currency, rate: Double): State[CurrencyExchangeRates, Option[Double]] =
     State[CurrencyExchangeRates, Option[Double]] { oldState =>
       val oldValue = oldState.dollarExchangeRates.get(to)
